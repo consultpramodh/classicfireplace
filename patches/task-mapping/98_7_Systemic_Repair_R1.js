18b643a7f4f091834e9298f4ab68093677f181c7
consultpramodh <consultpramodh@gmail.com>
2026-09-21 14:57:36 -0400

    Normalize systemic helper block newlines

--- patches/task-mapping/98_7_Systemic_Repair_R1.js
+++ patches/task-mapping/98_7_Systemic_Repair_R1.js
@@ -794,9 +794,18 @@ function tmSystemicWritePreInspectCalendarTaskLink_(eventId, taskId, taskTitle) 
   };
 }
 
-function tmSystemicEscapeHtml_(value) {\n  return String(value || '')\n    .replace(/&/g, '&amp;')\n    .replace(/</g, '&lt;')\n    .replace(/>/g, '&gt;')\n    .replace(/\"/g, '&quot;')\n    .replace(/'/g, '&#39;');\n}\n\nfunction tmSystemicEscapeRegex_(value) {\n  return String(value || '').replace(/[.*+?^$()|[\\]{}\\\\]/g, '\\\\$&');\n}\nfunction tmSystemicPauseRateWindow_(reason) {');
+function tmSystemicEscapeHtml_(value) {
+  return String(value || '')
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/\"/g, '&quot;')
+    .replace(/'/g, '&#39;');
 }
 
+function tmSystemicEscapeRegex_(value) {
+  return String(value || '').replace(/[.*+?^$()|[\\]{}\\\\]/g, '\\\\$&');
+}
 function tmSystemicPauseRateWindow_(reason) {
   Logger.log(
     'SYSTEMIC RATE WINDOW PAUSE: ' +
