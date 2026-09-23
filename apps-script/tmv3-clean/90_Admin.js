7dfba329d1f5a802ce056e50d7fbcc7f87d7eee2
consultpramodh <consultpramodh@gmail.com>
2026-09-23 12:41:08 -0400

    Task Mapping V3: add clean shadow foundation

---
+++ apps-script/tmv3-clean/90_Admin.js
@@ -0,0 +1,48 @@
+function tmv3_healthCheck() {
+  const requiredSheets = [
+    TMV3.SHEETS.OVERVIEW,TMV3.SHEETS.INSTALL,TMV3.SHEETS.DELIVERY,TMV3.SHEETS.SERVICE,
+    TMV3.SHEETS.PREINSPECTION,TMV3.SHEETS.STATE,TMV3.SHEETS.AUDIT,TMV3.SHEETS.CUSTOMERS,
+    TMV3.SHEETS.LOCATIONS,TMV3.SHEETS.CONTACTS,TMV3.SHEETS.ORDERS,TMV3.SHEETS.TASKS
+  ];
+  const ss = tmv3_ss_();
+  const missingSheets = requiredSheets.filter(function(n) { return !ss.getSheetByName(n); });
+  const caps = tmv3_configCapabilities_();
+  const missingCaps = Object.keys(caps).filter(function(k) { return !caps[k].present; });
+  const result = {
+    version:TMV3.VERSION,
+    mode:TMV3.MODE,
+    scriptId:ScriptApp.getScriptId(),
+    spreadsheetId:ss.getId(),
+    missingSheets:missingSheets,
+    missingPropertyCapabilities:missingCaps,
+    ready:missingSheets.length===0 && missingCaps.length===0
+  };
+  Logger.log(JSON.stringify(result));
+  return result;
+}
+
+function tmv3_configKeysOnly() {
+  return Object.keys(PropertiesService.getScriptProperties().getProperties()).sort();
+}
+
+function tmv3_importConfigBundle(bundle) {
+  if (!bundle || typeof bundle !== 'object') throw new Error('Configuration bundle required.');
+  const allowed = [
+    'CLIENT_ID','CLIENT_SECRET','Striven_Customers_ReportAPI','Striven_CustomerLocations_ReportAPI',
+    'Striven_Contacts_ReportAPI','Striven_ApprovedSalesOrders_ReportAPI','Striven_InstallTasks_ReportAPI',
+    'Striven_DeliveryTasks_ReportAPI','Striven_ServiceTasks_ReportAPI','Striven_ServiceWorkOrders_ReportAPI'
+  ];
+  const safe = {};
+  allowed.forEach(function(k) {
+    if (bundle[k] !== undefined && bundle[k] !== null && tmv3_clean_(bundle[k]) !== '') safe[k] = String(bundle[k]);
+  });
+  if (Object.keys(safe).length !== allowed.length) {
+    throw new Error('Config bundle incomplete. Expected ' + allowed.length + ' keys, received ' + Object.keys(safe).length + '.');
+  }
+  PropertiesService.getScriptProperties().setProperties(safe, false);
+  return { status:'IMPORTED', keys:Object.keys(safe).sort() };
+}
+
+function tmv3_assertNoBusinessWrites() {
+  return { mode:TMV3.MODE, businessWritesEnabled:false, calendarWritesEnabled:false, taskCreateEnabled:false, taskPatchEnabled:false };
+}
