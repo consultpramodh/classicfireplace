2b14ea18607aab3ba0c81bf8c554eefd35fc9269
consultpramodh <consultpramodh@gmail.com>
2026-09-25 14:11:05 -0400

    task-mapping: open one-task Rocco canary

--- apps-script/tmv3-clean/00_Config.js
+++ apps-script/tmv3-clean/00_Config.js
@@ -1,8 +1,8 @@
-const TMV3_VERSION = '3.10.9-post-goldcon-shadow-r2';
+const TMV3_VERSION = '3.11.0-rocco-canary-r1';
 const TMV3_EXECUTION_STAGE = 7;
 const TMV3_SPREADSHEET_ID = '1Rxo2t3QjlC7TFWNc3kQ8A2foBAxM0l0VcEtRh4fkU2E';
 const TMV3_TIMEZONE = 'America/Toronto';
-const TMV3_MODE = 'SHADOW_READ_ONLY';
+const TMV3_MODE = 'CANARY_WRITE';
 
 const TMV3 = Object.freeze({
   MODE: TMV3_MODE,
